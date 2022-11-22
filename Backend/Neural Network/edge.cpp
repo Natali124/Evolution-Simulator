617deadc81be2834a55ea24bcd8dc4b7a750fc44
Natali124 <nataligogishvili@gmail.com>
2022-11-22 18:56:17 +0100

    edge cpp and edge hpp files + content

---
+++ Backend/Neural Network/edge.cpp
@@ -0,0 +1,41 @@
+#include "edge.hpp"
+#pragma once
+
+Edge::Edge(){
+        //Natali: option to do it with setter - for now let's keep it this way so that initiation of prev_weight is always set correctly
+        weight = 0;
+        prev_weight = 0;
+        start_neuron = nullptr;
+        end_neuron = nullptr;
+    }
+Edge::Edge(double w):Edge(){
+        weight = w;
+    }
+Edge::Edge(double w, Neuron* start, Neuron* end):Edge(){
+        weight = w;
+        start_neuron = start;
+        end_neuron = end;
+    }
+void Edge::randomize_weight(){
+        double r = (double) rand() / RAND_MAX;
+        set_weight(r);
+    }
+double Edge::get_weight(){
+        return weight;
+    }
+Neuron* Edge::get_start_neuron(){
+        return start_neuron;
+    }
+Neuron* Edge::get_end_neuron(){
+        return end_neuron;
+    }
+void Edge::set_weight(double w){
+        prev_weight = weight;
+        weight = w;
+    }
+void Edge::set_start(Neuron* n){
+        start_neuron = n;
+    }
+void Edge::set_end(Neuron* n){
+        end_neuron = n;
+    }
---
+++ Backend/Neural Network/edge.hpp
@@ -0,0 +1,35 @@
+#include <random>
+
+class Neuron;
+
+
+class Edge{
+public:
+    //Constructors
+
+    Edge();
+    Edge(double w);
+    Edge(double w, Neuron* start, Neuron* end);
+
+    //Specific functions
+
+    void randomize_weight();
+
+    //Getters
+
+    double get_weight();
+    Neuron* get_start_neuron();
+    Neuron* get_end_neuron();
+
+    //Setters
+
+    void set_weight(double w); //also updates prev_weight
+    void set_start(Neuron* n);
+    void set_end(Neuron* n);
+
+private:
+    double weight;
+    double prev_weight;
+    Neuron* start_neuron;
+    Neuron* end_neuron;
+};
