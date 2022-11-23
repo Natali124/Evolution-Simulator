86a047e57a4e055bf044cd9a014d08c7b3e7ba2a
vincenzo-politelli <polivincenzo.02@gmail.com>
2022-11-23 16:13:15 +0100

    Update the files with the changes made in Backend

--- Evolution_1/environment.h
+++ Evolution_1/environment.h
@@ -1,6 +1,7 @@
 #ifndef ENVIRONMENT_H
 #define ENVIRONMENT_H
 
+#include <iostream>
 
 class Environment
 {
@@ -11,6 +12,41 @@ public:
     float get_height();
 private:
     float width, height;
+};
+
+enum Direction {up = 0, right = 1, down = 2, left = 3};
+
+// class Coordinate copied from td6
+class Coordinate {
+public:
+  Coordinate(double x_other, double y_other);
+  Coordinate();
+  Coordinate(const Coordinate &other);
+  ~Coordinate();
+
+  double get_x() const;
+  double get_y() const;
+  void set_x(const double x);
+  void set_y(const double y);
+  double get_distance(Coordinate other);
+
+  Coordinate operator+(const Coordinate& other);
+  Coordinate operator-();
+  Coordinate operator-(const Coordinate& other);
+
+  bool operator==(const Coordinate& other);
+  bool operator!=(const Coordinate& other);
+  bool operator>(const Coordinate& other);
+  bool operator<(const Coordinate& other);
+
+  static int get_num_instances();
+
+  friend std::ostream& operator<<(std::ostream& os, const Coordinate& c);
+
+private:
+  double x;
+  double y;
+  static int num_instances;
 };
 
 #endif // ENVIRONMENT_H
