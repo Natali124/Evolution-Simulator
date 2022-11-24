deea1c7697865b606fcf6b24b409014f6e6eaecf
anjamatic <anjamatic02@gmail.com>
2022-11-24 09:55:51 +0100

    Neural Network

--- Evolution_1/Neural_Network/neuron.hpp
+++ Evolution_1/Neural_Network/neuron.hpp
@@ -1,14 +1,36 @@
 #include <vector>
 #include "edge.hpp"
 
+
 class Neuron{
 public:
-    double get_value(){
-        return value;
-    }
+
+    // Constructors and destructur
+    Neuron(int neuron_id, vector<Edge> previous_edges, Layer layer); //constructor for hidden/output layer
+    Neuron(int neuron_id, vector<double> inputs, Layer layer); // constructor for input layer
+    ~Neuron();
+
+
+    // Getters
+    double get_value();
+    vector <Edge> get_previous_edges(); // returns input or previous edges 
+    vector<double> get_inputs();
+    vector <Edge> get_next_edges();
+
+    // Setters
+    void set_values();
+   
+
+    //Other functions  
     void forward_propagate(); //updates neuron based on neurons of the previous layer
-    vector <Edge> input_edges;
+
 private:
-    double value;
+    int neuron_id;
+   
+    vector <Edge> previous_edges; //incomming edges
+    vector <Edge> next_edges;  // output edges  
+    vector <double> inputs (0);
+    double value;  
     Layer layer;
+
 };
