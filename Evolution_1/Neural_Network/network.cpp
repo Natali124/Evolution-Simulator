54ca686cdc98bdac5ee036d596d981f92d5fd6d6
anjamatic <anjamatic02@gmail.com>
2022-11-25 10:50:10 +0100

    Added default constructor and fixed typo

--- Evolution_1/Neural_Network/network.cpp
+++ Evolution_1/Neural_Network/network.cpp
@@ -1,8 +1,8 @@
 #include "network.hpp"
 
 void Network::propagate(){
-    for (int i = 0; i < hidden_layer.size(); i++){
-        hidden_layer[i].forward_propagate();
+    for (int i = 0; i < hidden_layers.size(); i++){
+        hidden_layers[i].forward_propagate(); 
     }
     for (int i = 0; i < output_layer.size(); i++){
         output_layer[i].forward_propagate();
--- Evolution_1/Neural_Network/neuron.cpp
+++ Evolution_1/Neural_Network/neuron.cpp
@@ -7,41 +7,45 @@ using namespace std;
 
 //Constructors
 
-Neuron::Neuron(int neuron_id, vector<Edge> previous_edges, Layer layer){
-        this->neuron_id = neuron_id;
-        this ->previous_edges = previous_edges;
-        this ->layer = layer;
-        inputs = vector(0);
+Neuron::Neuron(){
+    previous_edges = nullptr;
+    layer =nullptr;
 
 }
 
-Neuron:: Neuron(int neuron_id, stdvector<double> inputs, Layer layer){
+Neuron::Neuron(int neuron_id, vector<Edge> previous_edges, Layer layer){
         this->neuron_id = neuron_id;
-        this ->inputs = inputs;
-        this ->layer = layer;
+        *(this ->previous_edges);
+        *(this ->layer);
+
+}
+
+Neuron:: Neuron(int neuron_id, Layer layer){
+        this->neuron_id = neuron_id;
+        *(this ->layer);
+        previous_edges = nullptr;
         }
 
 
-vector<double> Neuron :: get_inputs(){
-    return inputs;}
-
+//Getters
 vector<Edge> Neuron :: get_previous_edges(){
-    return previous_edges;}
+    return *previous_edges;}
 
 vector<Edge> Neuron :: get_next_edges(){
-    return next_edges;}
+    return *next_edges;}
 
+//Other functions
 void add_edge(Edge edge, bool previous){
     if(previous){
-        previous_edges.push_back(edge);}
+        (*previous_edges).push_back(edge);}
     else{
-        next_edges.push_back(edge);}}
+        (*next_edges).push_back(edge);}}
 
 
 void Neuron::forward_propagate(){
     double sum = 0;
-    for (int i = 0; i < previous_edges.size(); i ++){
-        sum += previous_edges[i].get_weight() * previous_edges[i].get_start_neuron()->value;
+    for (int i = 0; i < (*previous_edges).size(); i ++){
+        sum += (*previous_edges)[i].get_weight() * (*previous_edges)[i].get_start_neuron()->value;
     }
-    value = layer.f_activation(sum);
+    value = (*layer).f_activation(sum);
 }
