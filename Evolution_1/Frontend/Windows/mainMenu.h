a76f3111e59c496aba645eea02e61ec4cea17e9f
laurentiu-lazar <lazarlaurentiu2002@gmail.com>
2022-11-27 16:26:07 +0100

    Basic frontend classes
    Added basic frontend classes, together with placeholders for the following living_being (mouse) and environment (creature_display and its _scene). The main menu is supposed to be the main view for visualising our game and for now has some basic functionalities, as I figure out layouts and button connections.

---
+++ Evolution_1/Frontend/Windows/mainMenu.h
@@ -0,0 +1,26 @@
+#ifndef MAINMENU_H
+#define MAINMENU_H
+
+#include "creatureDisplay.h"
+#include <QMainWindow>
+
+//main window for displaying the game
+class MainMenu : public QMainWindow{
+    public:
+        MainMenu(QWidget *parent = nullptr);
+//        ~MainMenu();
+        CreatureDisplay* getDisplay(){
+            return &display;
+        }
+        void randomize_scene(){
+            display._scene.clear();
+            for(int i = 0; i < 10; i ++)
+                display.addRandomDot();
+        }
+
+    private:
+        CreatureDisplay display;
+        void setBackgroundImage(QString filePath);
+};
+
+#endif // MAINMENU_H
