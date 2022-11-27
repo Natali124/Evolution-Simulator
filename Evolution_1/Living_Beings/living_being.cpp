7370b5e8628af7d6069d0cbb0224671e6dfcd034
Pablo-Bertaud-Velten <pablo.bertaudvelten@gmail.com>
2022-11-27 14:47:49 +0100

    implemented basic shape, and bounding rectanle for each LB type
    plants are rectangles,
    preys are small circles
    predators are big circles

--- Evolution_1/Living_Beings/living_being.cpp
+++ Evolution_1/Living_Beings/living_being.cpp
@@ -1,6 +1,7 @@
 #include "environment.h"
 #include "living_being.h"
 
+#include <QGraphicsItem>
 #include <cmath>
 
 
@@ -14,6 +15,31 @@ LivingBeing::LivingBeing(){
     type = none;
     size = 0;
     position = Coordinate();
+}
+
+
+void LivingBeing::set_bounding_rect(qreal x, qreal y, qreal width, qreal height) { //top left corner at x,y
+    this->bounding_rect = QRectF(x, y, width, height);
+}
+
+void LivingBeing::set_shape() {   // for now preys and predators are circles, plants are rectangles
+    QPainterPath path;
+    if (this->type == 0) { // if the LB is a predator
+        this->set_bounding_rect(10, 10, 6, 6); // values just for testing purposes, to be changed
+        path.addEllipse((this->bounding_rect));
+    }
+
+    if (this->type == 1) { // if the LB is a prey
+        this->set_bounding_rect(10, 10, 3, 3); // values just for testing purposes, to be changed
+        path.addEllipse((this->bounding_rect));
+    }
+
+    else if (this->type == 2) { // if the LB is a plant
+        this->set_bounding_rect(10, 10, 2, 2); //values just for testing purposes, to be changed
+        path.addRect(10, 10, 2, 2);
+    }
+
+    this->shape = path;
 }
 
 
--- Evolution_1/Living_Beings/living_being.h
+++ Evolution_1/Living_Beings/living_being.h
@@ -4,6 +4,7 @@
 #include "environment.h"
 #include <iostream>
 #include <vector>
+#include <QGraphicsItem>
 
 
 
@@ -12,12 +13,18 @@
 const double Vision_ray_width = 0.02;
 
 
-class LivingBeing{
+class LivingBeing : public QGraphicsItem { //
 public:
   LivingBeing(); // By default Coordinate(), and type = none,  and size = 0
   LivingBeing(Coordinate position, float size);
   bool alive;
   float size;
+  QRectF bounding_rect; // the rectangle in which one will draw the shape() of the LB, might need to be a const
+  QPainterPath shape; // the shape inside bounding_rect(), might need to be a const
+  void set_bounding_rect(qreal x, qreal y, qreal width, qreal height); // bounding_rect will be initialized with top-left corner in x,y
+  void set_shape(); // sets the shape of the LB, everything is inside the bounding_rect
+
+
   Coordinate position;
   enum Type_LB{
       none = -1,
