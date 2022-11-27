5d10bf739924d810cf60c8ece7e5ba5b49c6d0b2
GarancePerrot <garance.perrot1@gmail.com>
2022-11-27 16:47:10 +0100

    plant functions
    the impact of plants  on the creature's body attributes when eaten

--- Evolution_1/Living_Beings/plant.cpp
+++ Evolution_1/Living_Beings/plant.cpp
@@ -1,6 +1,7 @@
 #include "plant.h"
 #include "living_being.h"
-
+#include "creature.h"
+#include<cmath>
 
 Plant::Plant() {
     reproduction_rate = 0;
@@ -9,3 +10,95 @@ Plant::Plant() {
 Plant::Plant(float reproduction_rate) {
     this-> reproduction_rate = reproduction_rate;
     type = plant; }
+
+void Plant::carbs(Creature c) {
+    float ps = size;
+    float cs = c.size;
+    float d = ps - cs;
+    float dif = abs(d);
+    float alpha = 1 - (1/dif);
+
+    c.energy += alpha;
+    c.physical_strength -= alpha;
+};
+
+void Plant::protein(Creature c){
+    float ps = size;
+    float cs = c.size;
+    float d = ps - cs;
+    float dif = abs(d);
+    float alpha = 1 - (1/dif);
+
+    c.physical_strength += alpha;
+    c.energy -= alpha;
+};
+
+void Plant::slimming_effect(Creature c){
+    float ps = size;
+    float cs = c.size;
+    float d = ps - cs;
+    float dif = abs(d);
+    float alpha = 1 - (1/dif);
+
+    c.eye_sight += alpha;
+    c.visibility -= alpha;
+};
+
+void Plant::allergenic_effect(Creature c){
+    float ps = size;
+    float cs = c.size;
+    float d = ps - cs;
+    float dif = abs(d);
+    float alpha = 1 - (1/dif);
+
+    c.visibility += alpha;
+    c.eye_sight -= alpha;
+};
+
+void Plant::allergenic_protein(Creature c) {
+    float ps = size;
+    float cs = c.size;
+    float d = ps - cs;
+    float dif = abs(d);
+    float alpha = 1 - (1/dif);
+
+    c.physical_strength += 2*alpha;
+    c.energy -=alpha;
+    c.eye_sight -= alpha;
+};
+
+void Plant::allergenic_carbs(Creature c){
+    float ps = size;
+    float cs = c.size;
+    float d = ps - cs;
+    float dif = abs(d);
+    float alpha = 1 - (1/dif);
+
+    c.energy += 2*alpha;
+    c.physical_strength -= alpha;
+    c.eye_sight -= alpha;
+};
+
+void Plant::slimming_protein(Creature c) {
+    float ps = size;
+    float cs = c.size;
+    float d = ps - cs;
+    float dif = abs(d);
+    float alpha = 1 - (1/dif);
+
+    c.physical_strength += 2*alpha;
+    c.energy -= alpha;
+    c.visibility -= alpha;
+};
+
+void Plant::slimming_carbs(Creature c) {
+    float ps = size;
+    float cs = c.size;
+    float d = ps - cs;
+    float dif = abs(d);
+    float alpha = 1 - (1/dif);
+
+    c.energy += 2*alpha;
+    c.physical_strength -= alpha;
+    c.visibility -= alpha;
+};
--- Evolution_1/Living_Beings/plant.h
+++ Evolution_1/Living_Beings/plant.h
@@ -1,6 +1,7 @@
 #ifndef PLANT_H
 #define PLANT_H
 #include "living_being.h"
+#include "creature.h"
 
 class Plant : public LivingBeing
 {
@@ -10,6 +11,23 @@ public:
     float reproduction_rate;
     Type_LB type;
     void reproduction();
+
+    //functions about a creature eating a plant:impact on physical_strength, energy, visibility and eye_sight.
+    //variables are incremented/ decremented by a coefficient alpha which depends on the
+    // difference between the size of the plant and the size of the creature. The higher this difference is,
+    // the less the plant will have an effect on the creature's body.
+    // not in coding language: alpha = 1 - (1/ dif(c_size , plant_size))
+    // ++ or -- means + or - 2*alpha
+
+    void carbs(Creature c);   // + energy , - strength (c becomes heavier)
+    void protein(Creature c); // + strength, - energy (hard to digest)
+    void slimming_effect(Creature c);// + eye_sight, - visibility (c becomes slimmer,easier to move the head)
+    void allergenic_effect(Creature c);// + visibility, - eye_sight (c has an allergic reaction: body swells)
+    void allergenic_protein(Creature c);// ++ strength , - energy and - visibility
+    void allergenic_carbs(Creature c);// ++ energy, - strength and - eye_sight
+    void slimming_protein(Creature c);//++ strength , - energy and - visibility
+    void slimming_carbs(Creature c);// ++ energy, - strength and - visibility
+
 };
 
 #endif // PLANT_H
