f0ce0db25106762e30048d5e1fbd01de25408c15
mathilde-cros <mathilde.cros02@gmail.com>
2022-12-04 21:12:09 +0100

    Tests on widget implementation + Syncing
    Syncing of QSlider and QProgressBar, Tests on mainwindow.iu on different possible layouts. (No commits on main branch, because not sure yet of correct implementation)

--- Evolution_1/main.cpp
+++ Evolution_1/main.cpp
@@ -2,10 +2,32 @@
 #include <QApplication>
 #include <QGraphicsScene>
 #include <iostream>
+#include <QHBoxLayout>
+#include <QSlider>
+#include <QProgressBar>
+#include <QCheckBox>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+
+    QWidget *mainWindow = new QWidget;
+    mainWindow->setWindowTitle("Choose properties of your new Creature");
+
+    QProgressBar *progress = new QProgressBar;
+    QSlider *slider = new QSlider(Qt::Horizontal);
+
+    progress->setRange(1,10);
+    slider->setRange(1,10);
+    progress->setValue(0);
+
+    QObject::connect(slider, SIGNAL(valueChanged(int)), progress, SLOT(setValue(int)));
+
+    QHBoxLayout *layout = new QHBoxLayout;
+    layout->addWidget(slider);
+    layout->addWidget(progress);
+    mainWindow->setLayout(layout);
+    mainWindow->show();
+
     MainMenu menu;
 
     menu.show();
