aec0c93c5a9d0d530daecf979632082810a1954c
vincenzo-politelli <polivincenzo.02@gmail.com>
2022-12-05 01:50:29 +0100

    Creation of the class barrier

--- Evolution_1/barrier.h
+++ Evolution_1/barrier.h
@@ -7,6 +7,15 @@ class Barrier: public QGraphicsItem
 {
 public:
     Barrier();
+    Barrier(QRectF block);
+
+    QRectF boundingRect() const;
+    QPainterPath shape() const;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option,
+                 QWidget *widget);
+
+    QRectF block;
+    QColor color;
 };
 
 #endif // BARRIER_H
