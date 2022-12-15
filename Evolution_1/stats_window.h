dcd8a2aa4e9ac860d71b6de8db359dca2fc8437d
GarancePerrot <garance.perrot1@gmail.com>
2022-12-15 09:13:05 +0100

    cmakelist for qtcharts
    should compile now

--- Evolution_1/stats_window.h
+++ Evolution_1/stats_window.h
@@ -19,7 +19,10 @@ class StatsWindow : public QMainWindow {
 #endif // STATS_WINDOW_H
 
 
-/*goals for this week, Pablo and Garance:
+/*goals for this week, Pablo and Garance: (changes made in the cmake list, should compile now)
+ *
+ * - install qtcharts module with the QT Maintenance Tool
+ *
  * - find how to get the input (attributes of living beings that are present in the environment)
  * - find ideas of what stats to analyze
  * - learn how to display simple graphs
