76f99506f26feb0597148083009ceda9b17314f6
mathilde-cros <mathilde.cros02@gmail.com>
2023-01-06 18:52:56 +0100

    Rectification of small details from the merging
    Small corrections

--- Evolution_1/main.cpp
+++ Evolution_1/main.cpp
@@ -3,11 +3,14 @@
 #include <QApplication>
 #include <iostream>
 #include <math.h>
+#include <mainwindow.h>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     SimulationView menu(new Environment());
+    MainWindow w;
+    w.show();
 
     return a.exec();
 }
