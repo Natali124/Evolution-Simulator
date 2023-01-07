b7a89bfd7f7b1fb663e5c96b945b73d3ce0a5a31
Victor-Mihaila <victor.mihaila@polytechnique.edu>
2023-01-07 21:37:19 +0200

    changed the bounding rectangle

--- Evolution_1/Living_Beings/living_being.cpp
+++ Evolution_1/Living_Beings/living_being.cpp
@@ -30,8 +30,8 @@ void LivingBeing::advance(int phase){
 QRectF LivingBeing::boundingRect() const
 {
     qreal adjust = 0.5;
-    return QRectF(-18 - adjust, -22 - adjust,
-                  36 + adjust, 60 + adjust);
+    return QRectF(-30 - adjust, -30 - adjust,
+                  60 + adjust, 60 + adjust);
 }
 
 QPainterPath LivingBeing::shape() const
