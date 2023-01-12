cbccac4078596e353c498c65a9bda4f07fd7e226
vincenzo-politelli <polivincenzo.02@gmail.com>
2023-01-12 08:37:04 +0100

    Continuing "manual merge"

--- Evolution_1/barrier.cpp
+++ Evolution_1/barrier.cpp
@@ -1,26 +1,6 @@
 #include "barrier.h"
-#include <QPainter>
-Barrier::Barrier(){
-    block = QRectF(0, 0, 1, 1); //rect with top left corner (0, 0) and (width, height) = (1, 1)
-    color = QColor(128, 128, 128); //grey
-}
 
-Barrier::Barrier(QRectF block){
-    this->block = block;
-}
+Barrier::Barrier()
+{
 
-QRectF Barrier::boundingRect() const {
-    return block;
-}
-
-QPainterPath Barrier::shape() const {
-    QPainterPath path;
-    path.addRect(-10, -20, 20, 40);
-    return path;
-}
-
-void Barrier::paint(QPainter *painter, const QStyleOptionGraphicsItem *option,
-                    QWidget *widget){
-    painter->setBrush(color);
-    painter->drawRect(block);
 }
--- Evolution_1/barrier.h
+++ Evolution_1/barrier.h
@@ -1,21 +1,11 @@
 #ifndef BARRIER_H
 #define BARRIER_H
 
-#include <QGraphicsItem>
 
-class Barrier: public QGraphicsItem
+class Barrier
 {
 public:
     Barrier();
-    Barrier(QRectF block);
-
-    QRectF boundingRect() const;
-    QPainterPath shape() const;
-    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option,
-                 QWidget *widget);
-
-    QRectF block;
-    QColor color;
 };
 
 #endif // BARRIER_H
