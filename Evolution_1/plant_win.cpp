092b4834c47b534237bf61918683c69e36047aa5
lorenzo-degiovanni <degiovanni.lorenzo@yahoo.com>
2023-01-20 23:18:02 +0100

    plant variable change

--- Evolution_1/plant_win.cpp
+++ Evolution_1/plant_win.cpp
@@ -6,37 +6,24 @@ Plant_win::Plant_win(QWidget *parent) :
     ui(new Ui::Plant_win)
 {
     ui->setupUi(this);
-    connect(ui->strength_c, SIGNAL(valueChanged(int)),ui->strength_s,SLOT(setValue(int)) );
-    connect(ui->strength_s,SIGNAL(valueChanged(int)),ui->strength_c,SLOT(setValue(int)) );
-
-    connect(ui->energy_c, SIGNAL(valueChanged(int)),ui->energy_s,SLOT(setValue(int)) );
-    connect(ui->energy_s,SIGNAL(valueChanged(int)),ui->energy_c,SLOT(setValue(int)) );
-
-    connect(ui->eyesight_c, SIGNAL(valueChanged(int)),ui->eyesight_s,SLOT(setValue(int)) );
-    connect(ui->eyesight_s,SIGNAL(valueChanged(int)),ui->eyesight_c,SLOT(setValue(int)) );
-
-    connect(ui->visibility_c, SIGNAL(valueChanged(int)),ui->visibility_s,SLOT(setValue(int)) );
-    connect(ui->visibility_s,SIGNAL(valueChanged(int)),ui->visibility_c,SLOT(setValue(int)) );
-
     connect(ui->health_c, SIGNAL(valueChanged(int)),ui->health_s,SLOT(setValue(int)) );
     connect(ui->health_s,SIGNAL(valueChanged(int)),ui->health_c,SLOT(setValue(int)) );
+
+    connect(ui->reproduction_c, SIGNAL(valueChanged(int)),ui->reproduction_s,SLOT(setValue(int)) );
+    connect(ui->reproduction_s,SIGNAL(valueChanged(int)),ui->reproduction_c,SLOT(setValue(int)) );
 
     connect(ui->size_c, SIGNAL(valueChanged(int)),ui->size_s,SLOT(setValue(int)) );
     connect(ui->size_s,SIGNAL(valueChanged(int)),ui->size_c,SLOT(setValue(int)) );
 
-    ui->strength_c->setStyleSheet("color: black; background-color: white");
-    ui->visibility_c->setStyleSheet("color: black; background-color: white");
     ui->size_c->setStyleSheet("color: black; background-color: white");
     ui->health_c->setStyleSheet("color: black; background-color: white");
-    ui->eyesight_c->setStyleSheet("color: black; background-color: white");
-    ui->energy_c->setStyleSheet("color: black; background-color: white");
+    ui->reproduction_c->setStyleSheet("color: black; background-color: white");
 
-    ui->strength_l->setStyleSheet("color: black");
-    ui->visibility_l->setStyleSheet("color: black");
+
     ui->size_l->setStyleSheet("color: black");
     ui->health_l->setStyleSheet("color: black");
-    ui->eyesight_l->setStyleSheet("color: black");
-    ui->energy_l->setStyleSheet("color: black");
+    ui->reproduction_l->setStyleSheet("color: black");
+
 
     ui->add_plant1->setStyleSheet("color: black; background-color: white");
     ui->groupBox->setStyleSheet("color: black");
