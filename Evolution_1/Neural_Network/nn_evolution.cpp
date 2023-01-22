4fdc153fe6e09638be29166fe7b35ea5c853f14b
Tita Rosemeyer <tita-philine.rosemeyer@polytechnique.edu>
2023-01-22 13:23:03 +0100

    Added saving networks functionality to nn_evolution
    - Now saves best, medium and worst network of first and last generation (for later comparison)
    - possible to disable this saving when calling run_evolution()

--- Evolution_1/Neural_Network/nn_evolution.cpp
+++ Evolution_1/Neural_Network/nn_evolution.cpp
@@ -25,7 +25,9 @@ double loss(vector<double> got, vector<double> expected){
       }
     //cout << sum << endl;
     return sum;
-   }
+   } else {
+      throw std::invalid_argument("Vectors don't have the same size!");
+    }
 }
 double get_loss(Network* nn, vector<double> input, vector<double> expected){
   // get loss of a network for a given input and expected vector
@@ -44,8 +46,12 @@ vector<vector<double>> get_random_vectors(int n_vectors, int size_vectors){
   return inputs;
 }
 
-double do_step(vector<Network*> &networks, int n, int tests,  double r, int n_in, int n_out){
-  // Do one step of the evolution, return average loss
+double do_step(vector<Network*> &networks, int n, int tests,  double r, int n_in, int n_out, bool save_networks, string safe_path, int crnt_gen){
+  // Do one step of the evolution, return average loss#
+
+  // bool save_networks = false; // whether to save best, medium and worst network
+  // string safe_path = ""; // where to save the networks
+  // int crnt_gen = 0; // current generation, only used for storing of networks
 
   // initializing network dictionary with loss
   vector<std::tuple<Network*, double>> network_loss = vector<std::tuple<Network*, double>>(0);
@@ -84,6 +90,15 @@ double do_step(vector<Network*> &networks, int n, int tests,  double r, int n_in
   int n_keep = (int) n / r;
   vector<Network*> new_networks = vector<Network*>(0);
 
+  if(save_networks){
+    Network* best_nn =  std::get<0>(network_loss[0]);
+    Network* medium_nn =  std::get<0>(network_loss[(int) (n-1)/2]);
+    Network* worst_nn = std::get<0>(network_loss[n-1]);
+    cout << "Saving networks" << endl;
+    best_nn->network_to_file(safe_path + "gen" + to_string(crnt_gen) +"_best.txt");
+    medium_nn->network_to_file(safe_path + "gen" + to_string(crnt_gen) + "_medium.txt");
+    worst_nn->network_to_file(safe_path + "gen" + to_string(crnt_gen) +"_worst.txt");
+   }
 
   // reproduce each of the first keep networks, each r times
   for (int i = 0; i < n_keep; i++) {
@@ -106,6 +121,10 @@ double do_step(vector<Network*> &networks, int n, int tests,  double r, int n_in
    return avg_loss;
 }
 
+double do_step(vector<Network*> &networks, int n, int tests,  double r, int n_in, int n_out){
+  return do_step(networks,n,tests,r,n_in,n_out,false,"",0);
+}
+
 void run_evolution(int n, double r, double tests, int n_in, int n_out, int n_gen, int print_every){
 //  int n = 100; // number of networks
 //  double r = 10; // number of children per network
@@ -116,7 +135,7 @@ void run_evolution(int n, double r, double tests, int n_in, int n_out, int n_gen
 //  int print_every = 10; // prints average loss of generation every print_every-th generation
   vector<Network*> networks = vector<Network*>(0);
   for (int i = 0; i < n; ++i) {
-      networks.push_back(new Network(n_in,n_out,2,7));
+      networks.push_back(new Network(n_in,n_out,1,n_in));
     }
 
   for (int gen = 0; gen < n_gen; ++gen) {
@@ -127,28 +146,35 @@ void run_evolution(int n, double r, double tests, int n_in, int n_out, int n_gen
     }
 }
 
-void run_evolution_in_file(int n, double r, double tests, int n_in, int n_out, int n_gen, int print_every, string filename){
+
+
+void run_evolution_in_file(int n, double r, double tests, int n_in, int n_out, int n_gen, int print_every, string filename, bool save_networks, string save_path){
 //  int n = 100; // number of networks
 //  double r = 10; // number of children per network
 //  double tests = 10; // number of test vectors per generation
 //  int n_in = 5; // size of input vectors
 //  int n_out = n_in; // size of output vectors (has to coincide with output of want_func!)
 //  int n_gen = 1000; // number of generations
-//  int print_every = 10; // prints average loss of generation every print_every-th generation
+//  int print_every = 10; // saves average loss of generation every print_every-th generation
+  //  string filename; // saves averages losses to filename
+  //  bool save_networks; // bool wether to save the best / medium/ worst networks of first and last generation
+  // string save_path; path to where the networks should be saved
   ofstream outfile (filename.c_str());
   cout << "Writing: "<< filename <<endl;
   outfile << n << endl << r << endl << tests << endl << n_in << endl << n_out <<endl << n_gen << endl << print_every <<endl;
   vector<Network*> networks = vector<Network*>(0);
   for (int i = 0; i < n; ++i) {
-      networks.push_back(new Network(n_in,n_out,2,7));
+      networks.push_back(new Network(n_in,n_out,1,n_in));
     }
   for (int gen = 0; gen < n_gen; ++gen) {
-      double avg_loss = do_step(networks,n,tests,r,n_in,n_out);
+      double avg_loss = do_step(networks,n,tests,r,n_in,n_out,save_networks && gen%(n_gen-1)==0,save_path,gen);
       if(gen % print_every == 0){
-      //cout << "Generation: " << gen << " Avg.Loss: " << avg_loss << endl;
       outfile << "Generation: " << gen << " Avg.Loss: " << avg_loss << endl;
         }
     }
   cout << "Finished!" << endl;
 
+}
+void run_evolution_in_file(int n, double r, double tests, int n_in, int n_out, int n_gen, int print_every, string filename){
+  run_evolution_in_file(n,r,tests,n_in,n_out,n_gen,print_every,filename,false,"");
 }
--- Evolution_1/Neural_Network/nn_evolution.hpp
+++ Evolution_1/Neural_Network/nn_evolution.hpp
@@ -18,7 +18,9 @@ double get_loss(Network* nn, vector<double> input, vector<double> expected);
 vector<vector<double>> get_random_vectors(int n_vectors, int size_vectors);
 
 double do_step(vector<Network*> &networks, int n, int tests,  double r, int n_in, int n_out);
+double do_step(vector<Network*> &networks, int n, int tests,  double r, int n_in, int n_out, bool save_networks, string safe_path, int crnt_gen);
 
 void run_evolution(int n = 100, double r = 10, double tests = 10, int n_in = 5, int n_out = 5, int n_gen = 1000, int print_every = 10);
 
 void run_evolution_in_file(int n, double r, double tests, int n_in, int n_out, int n_gen, int print_every, string filename);
+void run_evolution_in_file(int n, double r, double tests, int n_in, int n_out, int n_gen, int print_every, string filename, bool save_networks, string save_path);
